b3e651e7dac9b1a5d93c2e8cec1ba881de46ceab
Kiara Baxter <KiaraBaxter@wmpenn.edu>
2025-11-03 20:32:37 +0000

    update tree scaling

--- iwsdk-app/src/index.js
+++ iwsdk-app/src/index.js
@@ -69,7 +69,7 @@ World.create(document.getElementById('scene-container'), {
   //import 3d object (tree)
   const tree = AssetManager.getGLTF('mapleTree').scene;
   tree.position.set(5, -1, 5);
-  tree.scale.set(2, 2, 2);
+  tree.scale.set(0.5, 0.5, 0.5);
   const treeEntity = world.createTransformEntity(tree);
 
   //import 3d object (coin1)
